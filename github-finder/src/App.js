e8ee843baa4ce9a65e2314e06ce0032870e9b387
ulitol97 <eduulitol@gmail.com>
2021-02-16 02:07:07 +0100

    Base app ready.

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Alert from "./components/layout/Alert";
 import Navbar from "./components/layout/Navbar";
 import About from "./components/pages/About";
-import Search from "./components/users/Search";
+import NotFound from "./components/pages/About copy";
+import Home from "./components/pages/Home";
 import User from "./components/users/User";
-import Users from "./components/users/Users";
 import AlertState from "./context/alert/AlertState";
 import GithubState from "./context/github/GithubState";
 
@@ -20,22 +20,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={() => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                ></Route>
-                <Route
-                  exact
-                  path="/user/:login"
-                  render={(props) => <User {...props}></User>}
-                ></Route>
+                <Route exact path="/" component={Home}></Route>
+                <Route exact path="/user/:login" component={User}></Route>
                 <Route exact path="/about" component={About}></Route>
+                <Route component={NotFound}></Route>
               </Switch>
             </div>
           </div>
---
+++ github-finder/src/components/pages/About copy.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Not found</h1>
+      <p className="lead">This ain't the page you're looking for.</p>
+    </div>
+  );
+};
+
+export default NotFound;
